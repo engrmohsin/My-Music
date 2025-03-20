6cb6d1b5be1b08ae3e243949994d2a7f021e986a
engrmohsin <66106555+engrmohsin@users.noreply.github.com>
2025-03-20 15:42:56 +0500

    package json file
    package json file

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,13 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect('mongodb://127.0.0.1:27017/music_demo?authSource=admin', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(`Error1234: ${error.message}`);
     process.exit(1);
   }
 };
